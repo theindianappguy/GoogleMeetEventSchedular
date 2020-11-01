0e9aff30d2b8fe9805a47cd24aac4ee906471b79
theindianappguy <theindianappguy@gmail.com>
2020-11-01 19:44:42 +0530

    ....

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 class Secret {
-  static const ANDROID_CLIENT_ID = "";
-  static const IOS_CLIENT_ID = "";
+  static const ANDROID_CLIENT_ID =
+      "532346524547-vme6r9qh2pg3cebkhikacgpid1h5lhor.apps.googleusercontent.com";
+  static const IOS_CLIENT_ID =
+      "532346524547-7gdbh16gt050j53adqm07tcrgoshss69.apps.googleusercontent.com";
   static String getId() =>
       Platform.isAndroid ? Secret.ANDROID_CLIENT_ID : Secret.IOS_CLIENT_ID;
 }
